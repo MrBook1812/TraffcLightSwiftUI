8e08eb3d73a5ae377561369575d274bbcea7dfe3
Kirill Kniga <kiruha_95@icloud.com>
2023-04-22 15:35:19 +0300

    Initial Commit

---
+++ TraffcLightSwiftUI/TraffcLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraffcLightSwiftUIApp.swift
+//  TraffcLightSwiftUI
+//
+//  Created by Kirill Kniga on 22.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct TraffcLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
